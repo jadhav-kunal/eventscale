666b29f0d9eda93e4d3eea00cac9ca58022963a1
Kunal Jadhav <jadhavkunal1999@gmail.com>
2026-01-11 17:28:33 -0700

    chore: initialize Spring Boot app with health endpoint and basic security

---
+++ src/main/java/com/kunal/eventscale/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.kunal.eventscale.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "EventScale is running...";
+    }
+}
---
+++ src/test/java/com/kunal/eventscale/EventscaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.eventscale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventscaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
